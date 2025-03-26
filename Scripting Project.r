f56c0b94b1000a710a06c7f18ee1c7afc2040423
Cecilia Barnard <barnar3@clemson.edu>
2025-03-26 14:59:10 -0400

    downloaded files

--- Scripting Project.r
+++ Scripting Project.r
@@ -1,2 +1,19 @@
 Scripting Project
-library("internetarchive")
+library(internetarchive)
+library(dplyr)
+ia_list_fields()
+
+NLS_query <- c("contributor" = "National Library of Scotland", "language" = "iri")
+ia_search(NLS_query, num_results = 50)
+
+ats_query <- c("publisher" = "american tract society", "year" = "1864")
+ia_search(ats_query, num_results = 20)
+#this is the example model from opensci-I couldn't figure out why my code wasn't running so I copied this in
+#this ran and demonstrated that I had the syntax in ia_search wrong! 
+
+NLS_query <- c("contributor" = "National Library of Scotland", "language" = "iri")
+ia_search(NLS_query, num_results = 50)
+#this pulled results! Now let's try to download
+NLSdirectory <- tempdir() 
+ia_search(NLS_query, num_results = 50) %>% ia_get_items() %>% ia_files() %>% filter(type == "txt") %>% group_by(id) %>% ia_download(dir = NLSdirectory, overwrite = FALSE) %>% glimpse()
+#can't see the directory in R? I think I skipped a step? They're stored somewhere VERY weird
