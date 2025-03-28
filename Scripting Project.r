e4141ecdfc2db92591e85d6863e2b59efba1abb6
Cecilia Barnard <barnar3@clemson.edu>
2025-03-28 17:55:53 -0400

    starting over

--- Scripting Project.r
+++ Scripting Project.r
@@ -22,10 +22,57 @@ ia_search(NLS_query, num_results = 50) %>% ia_get_items() %>% ia_files() %>% fil
 NLS_query <- c("contributor" = "National Library of Scotland", "language" = "iri")
 ia_search(NLS_query, num_results = 50)
 
-NLS_metadata <- ia_search(NLS_query, num_results = 50) %>% ia_get_items() %>% ia_metadata %>% filter(field == "title") %>% select(value)
+NLS_metadata <- ia_search(NLS_query, num_results = 97) %>% ia_get_items() %>% ia_metadata  %>% filter(field == "title") %>% select(value)
 as_tibble(NLS_metadata)
-ia_files
+
+#because filtering wasn't doing what I wanted it to do necessarily, Amber and Mandolyn both advised to switch to pivoting and selecting 
 #there's a lot of degraded or outdated code in this package-it wanted me to create a tibble because the dataframe code doesn't work, so I put my ia_search code into as_tibble
 NLSdirectory <- tempdir() 
 ia_search(NLS_query, num_results = 50) %>% ia_get_items() %>% ia_files() %>% filter(type == "txt") %>% group_by(id) %>% ia_download(dir = NLSdirectory, overwrite = FALSE) %>% glimpse()
 #there is no weird file path-there's just a specific code to find the files: list.files(directoryname) that Mandolyn found! 
+list.files(NLSdirectory)
+#I went back in and removed the filter from my code after this-I want to see the full metadata tibble before I try to write my loop.
+#The full metadata tibble is a COMPLETE nightmare that I think it would take me all summer to fix correctly. 
+#But I did verify that it all downloaded, then went back in and filtered for title and language. 
+#The goal of my loop is to write one that separates out .txt files that have both English and Irish text.
+#this is me trying something
+ia_metadata("NLS_query")
+#yeah okay this didn't work
+#I'm having an issue with my filter so reinitiated tidyverse 
+#I think I did this wrong so I'm "starting over" aka not just mindlessly pulling stuff from the internet archive
+
+NLS_query <- c("contributor" = "National Library of Scotland", "language" = "iri")
+ia_search(NLS_query, num_results = 97)
+translationdf = data.frame()
+for (i in length(NLS_metadata$title))
+    if ("language" == "iri" & "language" == "eng")
+        next
+    if("language" == "iri" | "language" == "eng")
+        break
+    print(title)
+
+NLS_query <- c("contributor" = "National Library of Scotland", "language" = "iri")
+ia_search(NLS_query, num_results = 97)
+NLS_metadata <- ia_search(NLS_query, num_results = 97) %>% ia_get_items() %>% ia_metadata %>% select(value)
+as_tibble(NLS_metadata)
+metadata.filtered <- NLS_metadata %>% pivot_wider (names_from = field, values_from = value)
+#this took a very long time to figure out and Mandolyn came through with the solution for dataframes with one column
+NLS_metadata
+colnames(NLS_metadata) <- "title"
+for (i in length(NLS_metadata$title)) {
+    if ("language" == "eng") {
+    print(NLS_metadata$title) 
+     } else if ("language" != "eng") {
+    print(NULL)
+     }
+}
+#and this is where Amber reminded me that copilot exists
+#error: unexpected else in "    else"-let's ask copilot: it's a syntax error
+#copilot says to store in a dataframe which I was thinking about anyway
+#here's how it says to do it 
+for (i in seq_along(NLS_metadata$title)) {
+    if (language)
+}
+#STARTING OVER FROM THE BEGINNING 
+
+x
