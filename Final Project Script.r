26aebd133d67540056e4917f7a6d5b660d5176c2
Cecilia Barnard <barnar3@clemson.edu>
2025-04-07 15:00:30 -0400

    began final project, created metadata table

---
+++ Final Project Script.r
@@ -0,0 +1,11 @@
+Final Project Script #using info from our webscraping script project 
+library(internetarchive)
+library(tidyverse)
+
+Registrumquery <- c("contributor" = "National Library of Scotland", "title" = "Registrum")
+
+ia_search(Registrumquery, num_results = 19) %>% ia_get_items() %>% ia_metadata() %>% filter(field == "title" | field == "external-identifier")
+
+registrummetadata <- as.data.frame(ia_search(Registrumquery, num_results = 19) %>% ia_get_items() %>% ia_metadata() %>% filter(field == "title" | field == "external-identifier"))
+
+registrummetadata <- registrummetadata %>% pivot_wider(names_from = field, values_from = value)
