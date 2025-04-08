ee3a8bc4a1bf4106e73ed9fd64668f377f159e3f
Cecilia Barnard <barnar3@clemson.edu>
2025-04-08 11:43:24 -0400

    Topic Modeling first commit

--- Final Project Script.r
+++ Final Project Script.r
@@ -9,3 +9,7 @@ ia_search(Registrumquery, num_results = 19) %>% ia_get_items() %>% ia_metadata()
 registrummetadata <- as.data.frame(ia_search(Registrumquery, num_results = 19) %>% ia_get_items() %>% ia_metadata() %>% filter(field == "title" | field == "external-identifier"))
 
 registrummetadata <- registrummetadata %>% pivot_wider(names_from = field, values_from = value)
+
+dir.create("NLSParishRegisterTexts")
+
+ia_search(Registrumquery, num_results = 19) %>% ia_get_items %>% ia_files %>% filter(type == "txt") %>% group_by(id) %>% ia_download(dir = "NLSParishRegisterTexts", overwrite = FALSE,) %>% glimpse()
