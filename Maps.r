3dc52c03aa33be462dbe457611d89741328d624a
Cecilia Barnard <barnar3@clemson.edu>
2025-04-21 17:26:16 -0400

    first commit worksheet seven

---
+++ Maps.r
@@ -0,0 +1,10 @@
+Maps 
+library(ggmap)
+library(tidygeocoder)
+library(ggplot2)
+library(DigitalMethodsData)
+?geocode #geocode needs place data to run-city =, state =, method =, lat =, long =
+#watch what you're geocoding 
+?map_data()
+NHGIS data 
+
